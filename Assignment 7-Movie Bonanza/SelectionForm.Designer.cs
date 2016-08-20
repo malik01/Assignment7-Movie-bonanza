c461bca3befc67a047394993f432a9e55cbf1855
shahz <shahz@DESKTOP-A4RB8C0>
2016-08-19 21:27:51 -0400

    made the values appear with the picture and the cost and also selecting the title

--- Assignment 7-Movie Bonanza/SelectionForm.Designer.cs
+++ Assignment 7-Movie Bonanza/SelectionForm.Designer.cs
@@ -29,27 +29,27 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.movielistbox = new System.Windows.Forms.ListBox();
+            this.costtextbox = new System.Windows.Forms.TextBox();
+            this.titletextbox = new System.Windows.Forms.TextBox();
+            this.moviespictures = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.FirstAbilityComboBox = new System.Windows.Forms.ComboBox();
+            this.catagoryDropDown = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
             this.nextButton = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moviespictures)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox3);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.movielistbox);
+            this.groupBox1.Controls.Add(this.costtextbox);
+            this.groupBox1.Controls.Add(this.titletextbox);
+            this.groupBox1.Controls.Add(this.moviespictures);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
@@ -59,6 +59,40 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Your Selection";
+            // 
+            // movielistbox
+            // 
+            this.movielistbox.FormattingEnabled = true;
+            this.movielistbox.ItemHeight = 25;
+            this.movielistbox.Location = new System.Drawing.Point(201, 116);
+            this.movielistbox.Name = "movielistbox";
+            this.movielistbox.Size = new System.Drawing.Size(136, 154);
+            this.movielistbox.TabIndex = 6;
+            this.movielistbox.SelectedIndexChanged += new System.EventHandler(this.movielistbox_SelectedIndexChanged);
+            // 
+            // costtextbox
+            // 
+            this.costtextbox.Location = new System.Drawing.Point(357, 194);
+            this.costtextbox.Name = "costtextbox";
+            this.costtextbox.Size = new System.Drawing.Size(100, 30);
+            this.costtextbox.TabIndex = 5;
+            this.costtextbox.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // titletextbox
+            // 
+            this.titletextbox.Location = new System.Drawing.Point(201, 54);
+            this.titletextbox.Name = "titletextbox";
+            this.titletextbox.Size = new System.Drawing.Size(228, 30);
+            this.titletextbox.TabIndex = 4;
+            // 
+            // moviespictures
+            // 
+            this.moviespictures.Location = new System.Drawing.Point(6, 73);
+            this.moviespictures.Name = "moviespictures";
+            this.moviespictures.Size = new System.Drawing.Size(120, 169);
+            this.moviespictures.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.moviespictures.TabIndex = 3;
+            this.moviespictures.TabStop = false;
             // 
             // label3
             // 
@@ -72,92 +106,51 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(214, 166);
+            this.label2.Location = new System.Drawing.Point(212, 87);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(92, 25);
+            this.label2.Size = new System.Drawing.Size(75, 25);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Catagory";
+            this.label2.Text = "Movies";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(272, 73);
+            this.label1.Location = new System.Drawing.Point(272, 26);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(67, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "TITLE";
             // 
-            // pictureBox1
+            // catagoryDropDown
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(26, 73);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 169);
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(201, 101);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(228, 30);
-            this.textBox1.TabIndex = 4;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(188, 194);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(151, 30);
-            this.textBox2.TabIndex = 3;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(357, 194);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 30);
-            this.textBox3.TabIndex = 5;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
-            // 
-            // FirstAbilityComboBox
-            // 
-            this.FirstAbilityComboBox.BackColor = System.Drawing.Color.White;
-            this.FirstAbilityComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.FirstAbilityComboBox.FormattingEnabled = true;
-            this.FirstAbilityComboBox.Items.AddRange(new object[] {
-            "Cedar Rapids ",
-            "Company Men ",
-            "Death Race 2",
-            "Footloose",
-            "Gnomeo and Juliet ",
-            "I am Number Four",
-            "Just Go With It",
-            "No Strings Attached ",
-            "Real Steel",
-            "Sanctum ",
-            "Season of the Witch",
-            "The Dilemma ",
-            "The Eagle ",
-            "The Green Hornet",
-            "The Mechanic ",
-            "The Other Woman",
-            "The Rite ",
-            "The Roommate ",
-            "The Way Back ",
-            "Waiting for Forever "});
-            this.FirstAbilityComboBox.Location = new System.Drawing.Point(67, 42);
-            this.FirstAbilityComboBox.Margin = new System.Windows.Forms.Padding(4);
-            this.FirstAbilityComboBox.MaxDropDownItems = 6;
-            this.FirstAbilityComboBox.Name = "FirstAbilityComboBox";
-            this.FirstAbilityComboBox.Size = new System.Drawing.Size(217, 33);
-            this.FirstAbilityComboBox.TabIndex = 2;
+            this.catagoryDropDown.BackColor = System.Drawing.Color.White;
+            this.catagoryDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.catagoryDropDown.FormattingEnabled = true;
+            this.catagoryDropDown.Items.AddRange(new object[] {
+            "Comedy",
+            "Drama ",
+            "Action ",
+            "Sci-Fi ",
+            "Horror ",
+            "Thriller ",
+            "Family ",
+            "New Releases"});
+            this.catagoryDropDown.Location = new System.Drawing.Point(67, 42);
+            this.catagoryDropDown.Margin = new System.Windows.Forms.Padding(4);
+            this.catagoryDropDown.MaxDropDownItems = 6;
+            this.catagoryDropDown.Name = "catagoryDropDown";
+            this.catagoryDropDown.Size = new System.Drawing.Size(217, 33);
+            this.catagoryDropDown.TabIndex = 2;
+            this.catagoryDropDown.SelectedIndexChanged += new System.EventHandler(this.catagoryDropDown_SelectedIndexChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(13, 13);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(638, 25);
+            this.label4.Size = new System.Drawing.Size(703, 25);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Please Choose A Movie From The List Below That You Wish To Stream";
+            this.label4.Text = "Please Choose A Movie From The Categories Below That You Wish To Stream";
             // 
             // nextButton
             // 
@@ -174,18 +167,21 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(849, 452);
+            this.ControlBox = false;
             this.Controls.Add(this.nextButton);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.FirstAbilityComboBox);
+            this.Controls.Add(this.catagoryDropDown);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "SelectionForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SelectionForm";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moviespictures)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -194,15 +190,15 @@
         #endregion
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox costtextbox;
+        private System.Windows.Forms.TextBox titletextbox;
+        private System.Windows.Forms.PictureBox moviespictures;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.ComboBox FirstAbilityComboBox;
+        private System.Windows.Forms.ComboBox catagoryDropDown;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox movielistbox;
     }
 }
--- Assignment 7-Movie Bonanza/SelectionForm.cs
+++ Assignment 7-Movie Bonanza/SelectionForm.cs
@@ -12,18 +12,20 @@ namespace Assignment_7_Movie_Bonanza
 {
     public partial class SelectionForm : Form
     {
-        
+
         public OrderForm orderform = Program.ordercreen;
         public SelectionForm()
         {
             InitializeComponent();
         }
-        
+
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
+        decimal[] cost = new decimal[]
+        { 1.99m, 1.99m, 2.99m, 2.99m, 2.99m, 1.99m, 0.99m, 4.99m  };
         string[] catgory = new string[] { "Comedy", "Drama", "Action", "Sci-Fi", "Horror", "Thriller", "Family", "New Release" };
         string[] family = new string[] { "Gnomeo and Juliet" };
         string[] drama = new string[] { "Company Men", "The Way Back", "Waiting for Forever" };
@@ -32,16 +34,169 @@ namespace Assignment_7_Movie_Bonanza
         string[] horror = new string[] { "The Rite" };
         string[] newlyReleased = new string[] { "Footloose", "Real Steel" };
         string[] scifi = new string[] { "Season of the Witch", "I am Number Four" };
-        string[] action = new string[] {"The Green Hornet", "Death Race 2", "The Mechanic", "Sanctum", "The Other Woman", "The Eagle" };
+        string[] action = new string[] { "The Green Hornet", "Death Race 2", "The Mechanic", "Sanctum", "The Other Woman", "The Eagle" };
         private void FirstNameListBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void nextButton_Click(object sender, EventArgs e)
         {
             orderform.Show();
             this.Close();
+        }
+
+        private void catagoryDropDown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int category = catagoryDropDown.SelectedIndex;
+
+
+            if (category == 0)
+            {
+                movielistbox.Items.Clear();
+                movielistbox.Items.AddRange(comedy);
+                catagoryDropDown.Text = catgory[0];
+                costtextbox.Text = cost[0].ToString();
+            }
+            if (category == 1)
+            {
+                movielistbox.Items.Clear();
+                movielistbox.Items.AddRange(drama);
+                catagoryDropDown.Text = catgory[1];
+                costtextbox.Text = cost[0].ToString();
+            }
+            if (category == 2)
+            {
+                movielistbox.Items.Clear();
+                movielistbox.Items.AddRange(action);
+                catagoryDropDown.Text = catgory[2];
+                costtextbox.Text = cost[0].ToString();
+            }
+            if (category == 3)
+            {
+                movielistbox.Items.Clear();
+                movielistbox.Items.AddRange(scifi);
+                catagoryDropDown.Text = catgory[3];
+                costtextbox.Text = cost[0].ToString();
+            }
+            if (category == 4)
+            {
+                movielistbox.Items.Clear();
+                movielistbox.Items.AddRange(horror);
+                catagoryDropDown.Text = catgory[4];
+                costtextbox.Text = cost[0].ToString();
+            }
+            if (category == 5)
+            {
+                movielistbox.Items.Clear();
+                movielistbox.Items.AddRange(thriller);
+                catagoryDropDown.Text = catgory[5];
+                costtextbox.Text = cost[0].ToString();
+            }
+            if (category == 6)
+            {
+                movielistbox.Items.Clear();
+                movielistbox.Items.AddRange(family);
+                catagoryDropDown.Text = catgory[6];
+                costtextbox.Text = cost[0].ToString();
+            }
+            if (category == 7)
+            {
+                movielistbox.Items.Clear();
+                movielistbox.Items.AddRange(newlyReleased);
+                catagoryDropDown.Text = catgory[7];
+                costtextbox.Text = cost[7].ToString();
+            }
+        }
+
+        private void movielistbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            titletextbox.Text = string.Empty;
+
+            foreach (string value in movielistbox.SelectedItems)
+            {
+                titletextbox.Text = value.ToString();
+                if (value == "The Dilemma")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.thedilemma);
+                }
+                if (value == "No Strings Attached")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.nostringsattached);
+                }
+                if (value == "Cedar Rapids")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.cedarrapids);
+                }
+                if (value == "Just Go With it")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.justgowithit);
+                }
+                if (value == "Company Men")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.companymen);
+                }
+                if (value == "The Way Back")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.thewayback);
+                }
+                if (value == "Waiting for Forever")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.waitingforever);
+                }
+                if (value == "The Green Hornet")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.thegreenhornet);
+                }
+                if (value == "Death Race 2")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.deathrace2);
+                }
+                if (value == "The Mechanic")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.themechanic);
+                }
+                if (value == "Sanctum")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.sanctum);
+                }
+                if (value == "The Other Woman")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.theotherwoman);
+                }
+                if (value == "The Eagle")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.theeagle);
+                }
+                if (value == "Season of the Witch")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.seasonofthewitch);
+                }
+                if (value == "I am Number Four")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.iamnumberfour);
+                }
+                if (value == "The Rite")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.therite);
+                }
+                if (value == "The Roommate")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.theroommate);
+                }
+                if (value == "Gnomeo and Juliet")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.gnomeoandjuliet);
+                }
+                if (value == "Footloose")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.footloose);
+                }
+                if (value == "Real Steel")
+                {
+                    moviespictures.Image = new Bitmap(Properties.Resources.realsteel);
+                }
+            }
         }
     }
 }
